3fa2afd047561a35660ac5ff043b7c425b03b6a6
zl1509015035 <1509015035@qq.com>
2020-07-24 00:12:32 +0800

    Imooc

---
+++ src/com/Imooc/datamanage/DataManage.java
@@ -0,0 +1,36 @@
+package com.Imooc.datamanage;
+
+import java.util.Scanner;
+
+public class DataManage {
+    /**
+     * 从键盘接受数据
+     *
+     * @return 接收数据的数据
+     * @author zhul
+     */
+
+    public int[] insertData() {
+        int[] a = new int[10];
+        Scanner sc = new Scanner(System.in);
+        //少接受一个数据，为在指定位置处插入数据做准备
+        for (int i = 0; i < a.length - 1; i++) {
+            System.out.println("请输入第" + (i + 1) + "个数据:");
+            try{
+                a[i] = sc.nextInt();
+            }catch (java.util.InputMismatchException e){
+                System.out.println("输入的数据格式有误，不能有非数字！");
+                sc.next();
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        DataManage dm = new DataManage();
+        int[] a = dm.insertData();
+        for (int n : a) {
+            System.out.println(n+"  ");
+        }
+    }
+}
